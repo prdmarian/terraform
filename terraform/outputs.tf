36aeddcfb762b97bb633b1531f83d5abf15746ad
prdmarian <prd_marian@yahoo.com>
2024-10-21 17:17:45 +0300

    Delete terraform directory

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "k8s_master_ips" {
-  description = "IP addresses of Kubernetes master nodes"
-  value       = ["k8s-master-1", "k8s-master-2"]
-}
-
-output "k8s_worker_ips" {
-  description = "IP addresses of Kubernetes worker nodes"
-  value       = ["k8s-worker-1", "k8s-worker-2"]
-}
