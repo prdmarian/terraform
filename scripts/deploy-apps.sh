abea7ac617525da14324c8a4660bcbdaf600c7cd
prdmarian <prd_marian@yahoo.com>
2024-10-23 16:12:37 +0300

    update cert-manager

--- scripts/deploy-apps.sh
+++ scripts/deploy-apps.sh
@@ -8,29 +8,30 @@ then
 fi
 
 # Setează variabile pentru namespace-uri și certificare SSL
-CERT_ISSUER_NAME="letsencrypt-prod"
+CERT_ISSUER_NAME="letsencrypt"  # Dacă issuer-ul a rămas același
 EMAIL="admin@elgg.ro"
-CERT_NAMESPACE="cer-manager"
+CERT_NAMESPACE="cert-manager"
 REPO_URL="https://github.com/prdmarian/k8s-objects.git"
 CLONE_DIR="./k8s-objects"
 
 # 1. Crează namespace-urile necesare
 echo "Creare namespace-uri..."
-kubectl create namespace argocd
-kubectl create namespace monitoring
-kubectl create namespace letsencrypt
-kubectl create namespace tools
-kubectl create namespace web-apps
-kubectl create namespace ingress-proxy-internal
-kubectl create namespace ingress-proxy-external
+sudo kubectl create namespace argocd
+sudo kubectl create namespace monitoring
+sudo kubectl create namespace cert-manager
+sudo kubectl create namespace tools
+sudo kubectl create namespace web-apps
+sudo kubectl create namespace ingress-proxy-internal
+sudo kubectl create namespace ingress-proxy-external
 
 # 2. Adăugare repository-uri Helm
 echo "Adăugare Helm repositories pentru aplicații..."
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo add grafana https://grafana.github.io/helm-charts
-helm repo add jetstack https://charts.jetstack.io
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm repo update
+sudo helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+sudo helm repo add grafana https://grafana.github.io/helm-charts
+sudo helm repo add jetstack https://charts.jetstack.io
+sudo helm repo add bitnami https://charts.bitnami.com/bitnami
+sudo helm repo add argo https://argoproj.github.io/argo-helm
+sudo helm repo update
 
 # 3. Clonare repository-ul k8s-objects pentru aplicațiile ArgoCD
 if [ -d "$CLONE_DIR" ]; then
@@ -42,26 +43,26 @@ git clone $REPO_URL $CLONE_DIR
 
 # 4. Sincronizare aplicații cu ArgoCD (Cert-Manager, Ingress NGINX etc.)
 echo "Sincronizare aplicații cu ArgoCD..."
-kubectl apply -f $CLONE_DIR/applications/letsencrypt.yaml
-kubectl apply -f $CLONE_DIR/applications/prometheus.yaml
-kubectl apply -f $CLONE_DIR/applications/custom-exporter.yaml
-kubectl apply -f $CLONE_DIR/applications/grafana.yaml
-kubectl apply -f $CLONE_DIR/applications/ldap.yaml
-kubectl apply -f $CLONE_DIR/applications/git-cache.yaml
-kubectl apply -f $CLONE_DIR/applications/helm-cache.yaml
-kubectl apply -f $CLONE_DIR/applications/web-apps.yaml
-kubectl apply -f $CLONE_DIR/applications/ingress-proxy-internal.yaml
-kubectl apply -f $CLONE_DIR/applications/ingress-proxy-external.yaml
+sudo kubectl apply -f $CLONE_DIR/applications/cert-manager.yaml
+sudo kubectl apply -f $CLONE_DIR/applications/prometheus.yaml
+sudo kubectl apply -f $CLONE_DIR/applications/custom-exporter.yaml
+sudo kubectl apply -f $CLONE_DIR/applications/grafana.yaml
+sudo kubectl apply -f $CLONE_DIR/applications/ldap.yaml
+sudo kubectl apply -f $CLONE_DIR/applications/git-cache.yaml
+sudo kubectl apply -f $CLONE_DIR/applications/helm-cache.yaml
+sudo kubectl apply -f $CLONE_DIR/applications/web-apps.yaml
+sudo kubectl apply -f $CLONE_DIR/applications/ingress-proxy-internal.yaml
+sudo kubectl apply -f $CLONE_DIR/applications/ingress-proxy-external.yaml
 
 # 5. Verifică statusul deployment-urilor
 echo "Verificare status deployment-uri..."
-kubectl get pods -n argocd
-kubectl get pods -n monitoring
-kubectl get pods -n tools
-kubectl get pods -n letsencrypt
-kubectl get pods -n web-apps
-kubectl get pods -n ingress-proxy-internal
-kubectl get pods -n ingress-proxy-external
+sudo kubectl get pods -n argocd
+sudo kubectl get pods -n monitoring
+sudo kubectl get pods -n tools
+sudo kubectl get pods -n cert-manager
+sudo kubectl get pods -n web-apps
+sudo kubectl get pods -n ingress-proxy-internal
+sudo kubectl get pods -n ingress-proxy-external
 
 # 6. Finisare
 echo "Implementarea aplicațiilor și configurarea sunt complete!"
